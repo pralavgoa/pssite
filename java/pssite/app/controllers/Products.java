68fac9468f2683dd794c6423aa8ad05293f0e020
pralavgoa <pralavgoa@gmail.com>
2014-11-13 23:18:07 -0800

    project setup - complete example

--- java/pssite/app/controllers/Products.java
+++ java/pssite/app/controllers/Products.java
@@ -13,33 +13,47 @@ public class Products extends Controller{
 
 	private static final Form<Product> productForm = Form
 			.form(Product.class);
-	
+
 	public static Result list(){
 		List<Product> products = Product.findAll();
 		return ok(list.render(products));
 	}
-	
+
 	public static Result newProduct(){
 		return ok(details.render(productForm));
 	}	
-	public static Result details(String ean){
-		return TODO;
+	public static Result details(String ean) {
+		final Product product = Product.findByEan(ean);
+		if (product == null) {
+			return notFound(String.format("Product %s does not exist.", ean));
+		}
+		Form<Product> filledForm = productForm.fill(product);
+		return ok(details.render(filledForm));
 	}	
 	public static Result save(){
 		Form<Product> boundForm = productForm.bindFromRequest();
 		if(boundForm.hasErrors()) {
-		flash("error", "Please correct the form below.");
-		return badRequest(details.render(boundForm));
+			flash("error", "Please correct the form below.");
+			return badRequest(details.render(boundForm));
 		}
 		Product product = boundForm.get();
 		product.save();
 		flash("success",
-		String.format("Successfully added product %s", product));
+				String.format("Successfully added product %s", product));
 		return redirect(routes.Products.list());
 	}
-	
+
+	public static Result delete(String ean) {
+		final Product product = Product.findByEan(ean);
+		if(product == null) {
+			return notFound(String.format("Product %s does not exists.", ean));
+		}
+		Product.remove(product);
+		return redirect(routes.Products.list());
+	}
+
 	public static Result index(){
 		return TODO;
 	}
-	
+
 }
